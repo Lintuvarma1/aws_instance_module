f811966c25310c4c64349463fbce21d5748e9683
Ramakrishna_rk <151466917+Lintuvarma1@users.noreply.github.com>
2026-04-25 07:55:43 +0530

    Change security group configuration for AWS instance

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
     ami = var.instance_ami
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups = [var.security_group_id]
+    vpc_security_group_ids = [var.security_group_id]
     tags = var.instance_tags
   
 }
